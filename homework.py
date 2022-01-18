ae74b9ee33baf90c23856e35894e65797b35f957
KutyaninaAnastasia <92786427+KutyaninaAnastasia@users.noreply.github.com>
2022-01-18 20:02:45 +0300

    Project 6_v2

--- homework.py
+++ homework.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+import json
 import logging
 import os
 import time
@@ -37,7 +39,7 @@ def send_message(bot, message):
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logger.info('Отправлено сообщение.')
-    except Exception as error:
+    except telegram.TelegramError() as error:
         logger.error(f'Сообщение не отправлено. {error}')
 
 
@@ -52,44 +54,60 @@ def get_api_answer(current_timestamp):
             params=params,
             headers=HEADERS
         )
-    except TypeError as error:
+    except requests.exceptions.RequestException as error:
         raise Exception(f'Не верный тип данных {error}.')
-    if homework_answer.status_code != 200:
+    if homework_answer.status_code != HTTPStatus.OK:
         message = f'Эндпоинт {ENDPOINT} недоступен.'
         logger.error(message)
         raise Exception(message)
     logging.info("Получен ответ от сервера.")
-    return homework_answer.json()
+    try:
+        jhomework_answer = homework_answer.json()
+    except json.decoder.JSONDecodError:
+        raise Exception('Ответ должен быть преобразован в json ')
+    return jhomework_answer
 
 
 def check_response(response):
     """Проверяет ответ API на корректность."""
     if not isinstance(response, dict):
         raise TypeError('Ответ API не словарь.')
-    hw = ['homeworks'][0]
-    if hw not in response:
-        raise Exception('В ответе API нет домашней работы.')
-    homework = response.get('homeworks')[0]
-    # logger.info(type(homework))
+    homework = response['homeworks']
+    if not isinstance(homework, list):
+        raise TypeError('Ответ API не словарь.')
+    if homework is None:
+        raise Exception("Задания не обнаружены")
+    # if len(homework) == 0:
+    #     logger.debug('Нет изменений в статусах домашних работ')
+    #     raise ValueError('Нет изменений в статусах домашних работ')
+
     return homework
 
 
 def parse_status(homework):
     """Извлекает из информации о домашней работе статус этой работы."""
     if type(homework) == dict:
-        homework_name = homework['homework_name']
-        homework_status = homework['status']
+        if 'status' in homework:
+             homework_status = homework.get('status')
+        else:
+             raise KeyError(f'Ключа "status" нет в ответе')
+        if 'homework_name' in homework:
+             homework_name = homework.get('homework_name')
+        else:
+             raise KeyError(f'Ключа "homework_name" нет в ответе')
         if homework_status is None:
-            raise Exception('Пустое значение status.')
+             raise Exception('Пустое значение status.')
         if homework_name is None:
-            raise Exception('Пустое значение homework_name.')
-        if homework_status not in HOMEWORK_STATUSES:
+             raise Exception('Пустое значение homework_name.')
+        if homework_status in HOMEWORK_STATUSES:
+            verdict = HOMEWORK_STATUSES[homework_status]
+            return (f'Изменился статус проверки работы "{homework_name}" :'
+                    f' {verdict}')
+        else:
             message = 'Статуса домашней работы нет в словаре.'
             logger.error(message)
             raise Exception(message)
-        verdict = HOMEWORK_STATUSES[homework_status]
-        return f'Изменился статус проверки работы' \
-               f' "{homework_name}" : {verdict}'
+
     else:
         raise KeyError('Входной параметр не словарь.')
 
@@ -106,8 +124,8 @@ def main():
     """Основная логика работы бота."""
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     logger.debug('Бот запущен')
-    current_timestamp = 0
-    if check_tokens() is False:
+    current_timestamp = int(time.time())
+    if not check_tokens():
         message = 'Проверьте обязательные переменные окружения.'
         logger.critical(message)
         raise SystemExit(message)
@@ -116,10 +134,14 @@ def main():
             response = get_api_answer(current_timestamp)
             homework = check_response(response)
             if homework is not None:
-                status = parse_status(homework)
-                if status is not None:
-                    send_message(bot, status)
-                    logger.info('Сообщение о статусе работы отправлено')
+                if len(response) > 0:
+                    for hw in homework:
+                        status = parse_status(hw)
+                        if status is not None:
+                            send_message(bot, status)
+                            logger.info('Сообщение о статусе работы отправлено')
+                else:
+                    logger.debug('Нет изменений в статусах домашних работ')
             time.sleep(RETRY_TIME)
 
         except Exception as error:
